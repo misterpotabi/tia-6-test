544fa04c79a58eae81f18f585438f5df2214fd07
fajarhardeka <fajarhardeka@gmail.com>
2024-07-03 10:07:49 +0700

    update geden

--- sketch.js
+++ sketch.js
@@ -1,24 +1,34 @@
-let gantiWarna = false;
 function setup() {
-  createCanvas(800, 800, WEBGL);
+  createCanvas(1280, 960, WEBGL);
+  angleMode(DEGREES);
+  strokeWeight(3);
+  noFill();
+  stroke(32, 8, 64);
+  describe(
+    'Users can click on the screen and drag to adjust their perspective in 3D space. The space contains a sphere of dark purple cubes on a light pink background.'
+  );
 }
 
 function draw() {
-  background(200);
+  background(250, 180, 200);
 
-  //membuat fungsi orbit dengan mouse
+  // Call every frame to adjust camera based on mouse/touch
   orbitControl();
-  
-  //mengatur warna / pencahayaan
-  if (gantiWarna === true) {
-    let colors = color('orchid');
-    ambientLight(colors);
+
+  // Rotate rings in a half circle to create a sphere of cubes
+  for (let zAngle = 0; zAngle < 180; zAngle += 30) {
+    // Rotate cubes in a full circle to create a ring of cubes
+    for (let xAngle = 0; xAngle < 360; xAngle += 30) {
+      push();
+
+      // Rotate from center of sphere
+      rotateZ(zAngle);
+      rotateX(xAngle);
+
+      // Then translate down 400 units
+      translate(0, 400, 0);
+      box();
+      pop();
+    }
   }
-
-  sphere(150);
-}
-
-// membuat fungsi agar sphere bisa berubah warna
-function doubleClicked() {
-  gantiWarna = true;
 }
